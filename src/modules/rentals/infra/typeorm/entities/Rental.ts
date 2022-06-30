7f1277e7973b3602c02029e03585e19549cc46a4
Bruno Mateus <bruno_205@hotmail.com>
2022-06-30 05:41:14 -0300

    Rental

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -4,11 +4,11 @@ export class Rental {
   car_id: string;
   user_id: string;
   start_date: Date;
-  end_date: Date;
+  end_date?: Date;
   expected_return_date: Date;
   total: number;
   created_at: Date;
-  updated_at: Date;
+  updated_at?: Date;
 
   constructor() {
     if (!this.id) {
